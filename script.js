dba1236c41e330fe5963d664b14a778471b91ad6
Lucy Nguyen Pham <lucynpham@gmail.com>
2024-03-03 23:36:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var $divs = $('#text-box, #background');
+var sync = function(e){
+    var $other = $divs.not(this).off('scroll'), other = $other.get(0);
+    var percentage = this.scrollTop / (this.scrollHeight - this.offsetHeight);
+    other.scrollLeft = percentage * (other.scrollWidth - other.offsetWidth);
+    setTimeout( function(){ $other.on('scroll', sync ); },10);
+}
+$divs.on( 'scroll', sync);
+
+/* */
