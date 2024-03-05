1ae9f2de7c19d7685861ae17a43343fd332d2a9d
Lucy Nguyen Pham <lucynpham@gmail.com>
2024-03-05 02:28:30 -0500

    HUGE update!

--- script.js
+++ script.js
@@ -7,3 +7,50 @@ var sync = function(e){
 }
 $divs.on( 'scroll', sync);
 
+function article() {
+    var article = document.getElementById("text-box");
+    var listofgardens = document.getElementById("listofgardens");
+    var about = document.getElementById("about");
+
+    if (article.style.display === "none") {
+        article.style.display = "block";
+        listofgardens.style.display = "none";
+        about.style.display = "none";
+    } else {
+        article.style.display = "none";
+        listofgardens.style.display = "none";
+        about.style.display = "none";
+    }
+}
+
+function listofgardens() {
+    var article = document.getElementById("article");
+    var listofgardens = document.getElementById("listofgardens");
+    var about = document.getElementById("about");
+
+    if (listofgardens.style.display === "none") {
+        listofgardens.style.display = "block";
+        article.style.display = "none";
+        about.style.display = "none";
+    } else {
+        listofgardens.style.display = "none";
+        article.style.display = "block";
+        about.style.display = "none";
+    }
+}
+
+function about() {
+    var article = document.getElementById("article");
+    var listofgardens = document.getElementById("listofgardens");
+    var about = document.getElementById("about");
+
+    if (about.style.display === "none") {
+        about.style.display = "block";
+        article.style.display = "none";
+        listofgardens.style.display = "none";
+    } else {
+        listofgardens.style.display = "none";
+        article.style.display = "none";
+        about.style.display = "none";
+    }
+}
