f2c6b108a1f4031646e2f86d18c9dc73bc261add
Lucy Nguyen Pham <lucynpham@gmail.com>
2024-03-09 19:22:51 -0500

    Biiiiig stretch!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var $divs = $('#text-box, #background');
+var $divs = $('#text-box, #digital-garden');
 var sync = function(e){
     var $other = $divs.not(this).off('scroll'), other = $other.get(0);
     var percentage = this.scrollTop / (this.scrollHeight - this.offsetHeight);
@@ -7,19 +7,48 @@ var sync = function(e){
 }
 $divs.on( 'scroll', sync);
 
+/* For the Navigation! */
 function article() {
-    var article = document.getElementById("text-box");
+    var article = document.getElementById("article");
     var listofgardens = document.getElementById("listofgardens");
     var about = document.getElementById("about");
+    var textbox = document.getElementById("text-box");
+    var mobilemenulinks = document.getElementById("mobile-menu-links");
 
     if (article.style.display === "none") {
         article.style.display = "block";
+        textbox.style.display = "block";
         listofgardens.style.display = "none";
         about.style.display = "none";
+        mobilemenulinks.style.display = "none";
+    } else {
+        article.style.display = "block";
+        textbox.style.display = "none";
+        listofgardens.style.display = "none";
+        about.style.display = "none";
+        mobilemenulinks.style.display = "none";
+    }
+}
+
+function mobilearticle() {
+    var article = document.getElementById("article");
+    var listofgardens = document.getElementById("listofgardens");
+    var about = document.getElementById("about");
+    var textbox = document.getElementById("text-box");
+    var mobilemenulinks = document.getElementById("mobile-menu-links");
+
+    if (article.style.display === "none") {
+        article.style.display = "block";
+        textbox.style.display = "block";
+        listofgardens.style.display = "none";
+        about.style.display = "none";
+        mobilemenulinks.style.display = "none";
     } else {
         article.style.display = "none";
+        textbox.style.display = "none";
         listofgardens.style.display = "none";
         about.style.display = "none";
+        mobilemenulinks.style.display = "none";
     }
 }
 
@@ -27,15 +56,21 @@ function listofgardens() {
     var article = document.getElementById("article");
     var listofgardens = document.getElementById("listofgardens");
     var about = document.getElementById("about");
+    var textbox = document.getElementById("text-box");
+    var mobilemenulinks = document.getElementById("mobile-menu-links");
 
     if (listofgardens.style.display === "none") {
         listofgardens.style.display = "block";
+        textbox.style.display = "block";
         article.style.display = "none";
         about.style.display = "none";
+        mobilemenulinks.style.display = "none";
     } else {
         listofgardens.style.display = "none";
+        textbox.style.display = "none";
         article.style.display = "block";
         about.style.display = "none";
+        mobilemenulinks.style.display = "none";
     }
 }
 
@@ -43,14 +78,33 @@ function about() {
     var article = document.getElementById("article");
     var listofgardens = document.getElementById("listofgardens");
     var about = document.getElementById("about");
+    var textbox = document.getElementById("text-box");
+    var mobilemenulinks = document.getElementById("mobile-menu-links");
 
     if (about.style.display === "none") {
         about.style.display = "block";
+        textbox.style.display = "block";
         article.style.display = "none";
         listofgardens.style.display = "none";
+        mobilemenulinks.style.display = "none";
     } else {
         listofgardens.style.display = "none";
+        textbox.style.display = "none";
         article.style.display = "none";
         about.style.display = "none";
+        mobilemenulinks.style.display = "none";
     }
 }
+
+/* Mobile Menu */
+function openMenu() {
+    var x = document.getElementById("mobile-menu-links");
+    var article = document.getElementById("article");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      article.style.display = "block";
+    } else {
+      x.style.display = "block";
+      article.style.display = "none";
+    }
+  }
